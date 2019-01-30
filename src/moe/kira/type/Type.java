f46a0a9037a41d47e4301560aae73feb11feeeb7
Sotr <i@omc.hk>
2019-01-31 05:23:41 +0800

    Block data support

--- src/moe/kira/type/Type.java
+++ src/moe/kira/type/Type.java
@@ -25,11 +25,20 @@ package moe.kira.type;
 
 import static org.bukkit.Material.*;
 
+import java.lang.annotation.Documented;
 import java.util.EnumMap;
+import java.util.EnumSet;
 
 import org.bukkit.Material;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.type.Comparator;
+import org.bukkit.block.data.type.Comparator.Mode;
 
 public enum Type {
+    /**
+     * Multiple mappings to one material is unsupported.
+     */
+    
     /**
      * Traditional types.
      */
@@ -47,7 +56,11 @@ public enum Type {
     
     SAND(Material.SAND, RED_SAND),
     
+    AIR(Material.AIR, VOID_AIR, CAVE_AIR),
+    
     LOG(BIRCH_LOG, BIRCH_WOOD, JUNGLE_LOG, JUNGLE_WOOD, OAK_LOG, OAK_WOOD, SPRUCE_LOG, SPRUCE_WOOD, ACACIA_LOG, ACACIA_WOOD, DARK_OAK_LOG, DARK_OAK_WOOD),
+    
+    WOOL(BLACK_WOOL, BLUE_WOOL, BROWN_WOOL, CYAN_WOOL, GRAY_WOOL, GREEN_WOOL, LIGHT_BLUE_WOOL, LIGHT_GRAY_WOOL, LIME_WOOL, MAGENTA_WOOL, ORANGE_WOOL, PINK_WOOL, PURPLE_WOOL, RED_WOOL, WHITE_WOOL, YELLOW_WOOL),
     
     /**
      * <code>POLISHED_STONE</code><br>There are no polished stones.
@@ -55,13 +68,27 @@ public enum Type {
     STONE(Material.STONE, GRANITE, DIORITE, ANDESITE),
     
     /**
+     * Data types.
+     */
+    
+    REDSTONE_COMPARATOR_ON(COMPARATOR, (data) -> ((Comparator) data).getMode() == Mode.COMPARE),
+    
+    REDSTONE_COMPARATOR_OFF(COMPARATOR, (data) -> ((Comparator) data).getMode() == Mode.SUBTRACT),
+    
+    STATIONARY_WATER(Material.WATER),
+    
+    WATER(Material.WATER),
+    
+    /**
      * New types.
      */
     
     POLISHED_STONE(SMOOTH_STONE, POLISHED_GRANITE, POLISHED_DIORITE, POLISHED_ANDESITE),
     
+    WOOD(BIRCH_WOOD, JUNGLE_WOOD, OAK_WOOD, SPRUCE_WOOD, ACACIA_WOOD, DARK_OAK_WOOD),
+    
     /**
-     * Aliases and references.
+     * Aliases.
      */
     
     /**
@@ -80,94 +107,172 @@ public enum Type {
     WATER_LILY(LILY_PAD),
     
     /**
-     * <code>WATER</code><br>This is an alias.
+     * <code>NETHER_PORTAL</code><br>This is an alias.
      */
-    STATIONARY_WATER(Material.WATER),
+    PORTAL(NETHER_PORTAL),
     
     /**
-     * <code>WATER</code><br>This is an reference.
+     * <code>GUNPOWDER</code><br>This is an alias.
      */
-    WATER(Material.WATER),
+    SULPHUR(GUNPOWDER),
     
     /**
-     * <code>VINE</code><br>This is an reference.
+     * <code>VINE</code><br>This is an alias.
      */
     VINE(Material.VINE),
     
     /**
-     * <code>GRAVEL</code><br>This is an reference.
+     * <code>GRAVEL</code><br>This is an alias.
      */
     GRAVEL(Material.GRAVEL),
     
     /**
+     * Bad types.
+     */
+    
+    /**
      * This type have not been covered yet.
      */
-    OTHERS;
+    OTHERS,
+    
+    /**
+     * This type is unexpected.
+     */
+    UNWANTED;
     
     /**
      * Public area.
      */
     
-    /**
-     * @param material
-     * @return
-     */
+    @Nondata
+    public final static boolean is(Material material, Type type) {
+        return type.is(material);
+    }
+    
+    @Nondata
     public final boolean is(Material material) {
         return predicate.is(material);
     }
     
-    /**
-     * @param material
-     * @return
-     */
+    @Nondata
+    public final boolean is(Material material, Type... types) {
+        for (Type type : types)
+            if (type.is(material))
+                return true;
+        
+        return false;
+    }
+    
+    @Nondata
     public final static Type of(Material material) {
-        return TypeUnit.reverseMap.getOrDefault(material, OTHERS);
+        EnumSet<Type> types = TypeUnit.reverseMap.get(material);
+        
+        switch (types.size()) {
+            case 0:
+                return OTHERS;
+            case 1:
+                return types.iterator().next();
+            default:
+                return UNWANTED;
+        }
+    }
+    
+    public final static boolean is(BlockData data, Type type) {
+        return type.is(data);
+    }
+    
+    public final static boolean is(BlockData data, Type... types) {
+        for (Type type : types)
+            if (type.is(data))
+                return true;
+        
+        return false;
+    }
+    
+    public final boolean is(BlockData data) {
+        return predicate.is(data.getMaterial()) ? (hasData ? predicateData.is(data) : false) : false;
+    }
+    
+    public final static Type of(BlockData data) {
+        EnumSet<Type> types = TypeUnit.reverseMap.get(data.getMaterial());
+        
+        if (types == null)
+            return OTHERS;
+        
+        Type optimum = UNWANTED;
+        for (Type type : types)
+            if (type.is(data))
+                if (optimum == null)
+                    optimum = type;
+                else if (type.hasData)
+                        optimum = type;
+        
+        assert optimum != UNWANTED : "Unexpected Type: " + data.getAsString();
+        return optimum;
+    }
+    
+    @Documented
+    public static @interface Nondata {
     }
     
     /**
      * Hidden infrastructures.
      */
-    
-    private final TrueType predicate;
-    
-    private Type(Material material) {
-        predicate = TypeUnit.of((in) -> in == material);
-        TypeUnit.reverseMap.put(material, this);
+
+    private final TrueType<Material> predicate;
+
+    private boolean hasData;
+    private TrueType<BlockData> predicateData;
+
+    private Type() {
+        predicate = TypeUnit.of((in) -> false);
     }
-    
+
+    private Type(Material material, TrueType<BlockData> dataPredicate) {
+        this(material);
+        predicateData = dataPredicate;
+    }
+
+    private Type(Material material) {
+        predicate = TypeUnit.of((in) -> material == in);
+        reverseType(material);
+    }
+
     private Type(Material... materials) {
         predicate = TypeUnit.of((in) -> contains(materials, in));
+        
         for (Material material : materials)
-            TypeUnit.reverseMap.put(material, this);
+            reverseType(material);
     }
-    
+
+    private void reverseType(Material key) {
+        EnumSet<Type> types = TypeUnit.reverseMap.get(key);
+        
+        if (types == null) {
+            TypeUnit.reverseMap.put(key, EnumSet.of(this));
+        } else {
+            types.add(this);
+        }
+    }
+
     private static final boolean contains(Material[] array, Material dest) {
         for (Material material : array)
             if (material == dest)
                 return true;
+        
         return false;
     }
-    
+
     @FunctionalInterface
-    private interface TrueType {
-        boolean is(Material t);
+    private interface TrueType<T> {
+        boolean is(T material);
     }
     
-    private static final class TypeUnit implements TrueType {
-        private static final EnumMap<Material, Type> reverseMap = new EnumMap<Material, Type>(Material.class);
-        private final TrueType predicate;
+    private static final class TypeUnit {
+        private static final EnumMap<Material, EnumSet<Type>> reverseMap = new EnumMap<Material, EnumSet<Type>>(Material.class);
         
-        private TypeUnit(TrueType predicate) {
-            this.predicate = predicate;
-        }
-        
-        private final static TrueType of(TrueType predicate) {
-            return new TypeUnit(predicate);
-        }
-        
-        @Override
-        public final boolean is(Material material) {
-            return predicate.is(material);
+        private final static <T> TrueType<T> of(TrueType<T> predicate) {
+            return predicate;
         }
     }
 }
